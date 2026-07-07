090dcb7414cec8713a226009f8e54324d2352077
Paulo Lunardi <lunardi.hb@gmail.com>
2026-07-06 23:51:12 -0300

    Create 006_Formas_Alternativas.php for variable comparison
    Add PHP script to compare two variables using alternative syntax.

---
+++ Dev_Web_Html5_Css_Javascript_php/PHP/006_Formas_Alternativas.php
@@ -0,0 +1,15 @@
+<?php
+
+ $var1 = 10;
+ $var2 = 10;
+
+ if($var1 > $var2):
+	echo "$var1 é maior que $var2";
+ elseif($var1 < $var2):
+	echo "$var1 é menor que $var2";
+ else:
+	echo "$var1 e $var2 são iguais";
+ endif;
+
+// PHP permite que sejam utilizadas formas alternativas das instruções vistas. 
+// Em linhas gerais, troca-se a chave de abertura por dois pontos e a de fechamento pela palavra reservada “end” seguida do nome da instrução.
