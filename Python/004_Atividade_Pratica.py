0fd6152e9aa4db114aa4f3f2c873af0f9b9be943
Paulo Lunardi <lunardi.hb@gmail.com>
2026-07-20 00:53:55 -0300

    Add input conversion for integer, float, and boolean

---
+++ Python/004_Atividade_Pratica.py
@@ -0,0 +1,14 @@
+
+num_inteiro = int(input("Digite um numero inteiro: "))
+
+num_ponto_flutuante = float(input("Digite um número de ponto flutuante:"))
+
+booleano = input("Digite um valor booleano (True ou False): ") == "True"
+
+print("Valores convertidos: ")
+print(f"Número inteiro: {num_inteiro} (tipo: int)")
+print(f"Número de ponto flutuante: {num_ponto_flutuante} (tipo: float)")
+print(f"Valor booleano: {booleano} (tipo: bool)")
+
+
+
