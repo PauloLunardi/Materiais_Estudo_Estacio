06bf051501df68dd22660331e04c28b531f1c26a
Paulo Lunardi <lunardi.hb@gmail.com>
2026-07-28 02:06:24 -0300

    Add comment for transaction data access in gerar_extrato

--- Python/POO/016_Implementando_Heranca/Extrato.py
+++ Python/POO/016_Implementando_Heranca/Extrato.py
@@ -6,4 +6,5 @@ class Extrato:
   def gerar_extrato(self, conta):
     print(f"Extrato da conta {conta}")
     for tran in self.transacoes:
+      # Acesse: tran[0] para o tipo, tran[1] para o valor e tran[2] para a data
       print(f"{tran[0]:15s} {tran[1]:10.2f} {tran[2].strftime('%d/%b/%y')}")
