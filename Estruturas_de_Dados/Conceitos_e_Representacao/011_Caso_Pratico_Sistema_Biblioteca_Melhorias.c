e719c9f9d4ecff1bda0bc18e9928f7541cd47ba5
Paulo Lunardi <lunardi.hb@gmail.com>
2026-08-03 17:27:05 -0300

    Add book loan functionality to library system
    Implement new functionality for book loans in the library system, including basic book information and user loan management.

---
+++ Estruturas_de_Dados/Conceitos_e_Representacao/011_Caso_Pratico_Sistema_Biblioteca_Melhorias.c
@@ -0,0 +1,7 @@
+/*
+Nesta prática, você aplicará os conhecimentos adquiridos no nível aventureiro de estrutura de dados para implementar a evolução do sistema de uma biblioteca para incluir uma nova funcionalidade: o empréstimo de livros.
+Além de registrar informações básicas de cada livro (nome, autor, editora e edição).
+O sistema também permitirá que usuários realizem e consultem empréstimos. 
+Para isso, você utilizará ponteiros, alocação dinâmica de memória com malloc e calloc e gerenciará a memória com free.
+*/
+
