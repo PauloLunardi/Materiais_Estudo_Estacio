57e01baba31a2fd7d01903ff3d0017c923822946
Paulo Lunardi <lunardi.hb@gmail.com>
2026-08-05 15:27:35 -0300

    Implement world map and battle actions
    Added a world map and battle menu for the game.

--- Estruturas_de_Dados/Conceitos_e_Representacao/018__Desafio_Nivel_Mestre.c
+++ Estruturas_de_Dados/Conceitos_e_Representacao/018__Desafio_Nivel_Mestre.c
@@ -1,1 +1,21 @@
-xxx
+/*
+================== MAPA DO MUNDO ====================
+1. America      (Exercito: Verde            ,Tropas: 5)
+2. Europa       (Exercito: Azul             ,Tropas: 3)
+3. Asia      (Exercito: Vermelho            ,Tropas: 2)
+4. Africa       (Exercito: Amarelo          ,Tropas: 4)
+5. Oceania       (Exercito: Branco          ,Tropas: 1)
+=====================================================
+
+menu de acoes
+1 atacar
+2 verificar missao
+0 sair
+
+fase de ataque 
+  escolher o territorio de atacante
+  escolher o territorio de defensor 
+
+--- resultado da batalha ---
+  Ataque(europa):4 | Defesa (america): 5
+  VITORIA DA DEFESA! O atacante perdeu 1 tropa.
